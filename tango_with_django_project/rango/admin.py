52308e071a051930f032542fcb3a3f1debe3505b
YangGao1208 <2840606g@student.gla.ac.uk>
2024-02-08 18:41:52 +0000

    commit 4

--- tango_with_django_project/rango/admin.py
+++ tango_with_django_project/rango/admin.py
@@ -1,3 +1,16 @@
 from django.contrib import admin
+from rango.models import Category, Page
 
 # Register your models here.
+
+
+@admin.register(Page)
+class PageAdmin(admin.ModelAdmin):
+    list_display = ['title', 'Category', 'url']
+    
+
+
+admin.site.register(Category)
+
+
+
--- tango_with_django_project/tango_with_django_project/urls.py
+++ tango_with_django_project/tango_with_django_project/urls.py
@@ -20,6 +20,7 @@ from django.conf import settings
 from django.conf.urls.static import static
 
 
+
 urlpatterns = [
     path('rango/', include('rango.urls')),
     path('admin/', admin.site.urls),
