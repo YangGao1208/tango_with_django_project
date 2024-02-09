fd61f69fa95edb7b762c25e561ba753d4a5d770d
YangGao1208 <2840606g@student.gla.ac.uk>
2024-02-09 18:16:59 +0000

    Commit 9

--- tango_with_django_project/rango/views.py
+++ tango_with_django_project/rango/views.py
@@ -7,6 +7,7 @@ from rango.forms import PageForm
 from django.shortcuts import redirect
 from django.urls import reverse
 from rango.forms import UserForm, UserProfileForm
+from datetime import datetime
 # Create your views here.
 # def index(request):
 #     context_dict = {'boldmessage':'Crunchy,creamy,cookie,candy,cupcake!'}
@@ -138,4 +139,18 @@ def register(request):
         
         
     return render(request, 'rango/register.html', context = {'user_form' : user_form, 'profile_form': profile_form, 'registered': registered})
+
+
+
+
+def visitor_cookie_handler(request, response):
+    visits = int(request.COOKIES.get('visits', '1'))
+    last_visit_cookie = request.COOKIES.get('last_visit', str(datetime.now()))
+    last_visit_time = datetime.strptime(last_visit_cookie[:-7],'%Y-%m-%d %H:%M:%S')
+    if (datetime.now() - last_visit_time).days > 0:
+        visits = visits + 1
+        response.set_cookie('last_visit', str(datetime.now()))
+    else:
+        response.set_cookie('last_visit', last_visit_cookie)
+        response.set_cookie('visits', visits)
      
